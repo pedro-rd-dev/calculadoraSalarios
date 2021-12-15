3bbd750286c859bd98e7f560f79e064c04d1d393
sala19a <pedroh.mix>
2021-12-15 20:16:58 -0300

    Iniciando o projeto

---
+++ src/com/senac/IniciarProjeto.java
@@ -0,0 +1,23 @@
+package com.senac;
+
+public class IniciarProjeto {
+
+    public static void main(String[] args) {
+
+        double salarioJuniorHora = 25.00;
+        double salarioPlenoHora = 45.00;
+        double salarioSeniorHora = 60.00;
+        int quantidadeHorasTrabalhadasNoDia = 6;
+        int quantidadeDeDiasTralhadosNoMes = 30;
+
+        double salarioJuniorMes = salarioJuniorHora * quantidadeHorasTrabalhadasNoDia * quantidadeDeDiasTralhadosNoMes;
+        double salarioPlenoMes = salarioPlenoHora * quantidadeHorasTrabalhadasNoDia * quantidadeDeDiasTralhadosNoMes;
+        double salarioSeniorMes = salarioSeniorHora * quantidadeHorasTrabalhadasNoDia * quantidadeDeDiasTralhadosNoMes;
+
+
+        System.out.println();
+
+    }
+
+
+}
